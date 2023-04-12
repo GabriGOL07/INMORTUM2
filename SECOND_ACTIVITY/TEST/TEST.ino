222cb267f977f10de64c4382cac6c8ec94dba696
GabriGOL07 <gabrielq.escola@gmail.com>
2023-04-12 17:58:11 +0200

    #error en noms de fitxers

--- SECOND_ACTIVITY/TEST/TEST.ino
+++ SECOND_ACTIVITY/TEST/TEST.ino
@@ -10,13 +10,8 @@ const int interruptor5a = 10;
 const int interruptor5b = 11;
 const int interruptor6a = 12;
 const int interruptor6b = 13;
-const int ajuda = 1;
-const int led1 = A0;
-const int led2 = A1;
-const int led3 = A2;
-const int led4 = A3;
-const int led5 = A4;
-const int sirena = A5;
+const int ajuda = 0;
+
 
 int estado1a;
 int estado1b;
@@ -31,8 +26,6 @@ int estado5b;
 int estado6a;
 int estado6b;
 int estat_ajuda;
-
-int state = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -49,16 +42,9 @@ void setup() {
   pinMode(interruptor6a, INPUT_PULLUP);
   pinMode(interruptor6b, INPUT_PULLUP);
   pinMode(ajuda,INPUT_PULLUP);
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
-  pinMode(led4,OUTPUT);
-  pinMode(led5,OUTPUT);
-  pinMode(sirena,OUTPUT);
-  
 }
 
-void comprovar_estat(){
+void loop() {
   estado1a = digitalRead(interruptor1a);
   estado1b = digitalRead(interruptor1b);
   estado2a = digitalRead(interruptor2a);
@@ -98,59 +84,5 @@ void comprovar_estat(){
   Serial.println(estado6b);
   Serial.print("Interruptor ajuda: ");
   Serial.println(estat_ajuda);
-  delay(1000);
-}
-
-void leds(){
-  for (int i=0; i<20 ;i++){
-    digitalWrite(led1, HIGH);
-    delay(100);
-    digitalWrite(led1, LOW);
-    digitalWrite(led2, HIGH);
-    delay(100);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, HIGH);
-    delay(100);
-    digitalWrite(led3, LOW);
-    digitalWrite(led4, HIGH);
-    delay(100);
-    digitalWrite(led4, LOW);
-    digitalWrite(led5, HIGH);
-    delay(100);
-    digitalWrite(led5, LOW);
-  }
-  
-}
-void loop() {
-  
-  switch (state){
-    case 0: // no esta correcte la combinació
-      digitalWrite(sirena, LOW);
-      //Serial.println("Estat 0");
-      digitalWrite(led1, LOW);
-      comprovar_estat();
-      if  (estado1a == LOW && estado1b == HIGH && estado2a == LOW && estado2b == HIGH && estado3a == LOW && estado3b == HIGH && estado4a == LOW && estado4b == HIGH && estado5a == LOW && estado5b == HIGH && estado6a == LOW && estado6b == HIGH || ajuda == LOW){
-        state = 1;
-      }else{
-        state = 0;
-      }
-      break;     
-    case 1: // acertada combinació
-      //Serial.println("Estat 1");
-      digitalWrite(sirena, HIGH);
-      leds();      
-      digitalWrite(sirena, LOW);
-      state = 2;
-      break;
-    case 2: // ha acabat la sirena i els leds pero segueixen els pusladors al seu lloc, falta reiniciar
-      //Serial.println("Estat 2");
-      digitalWrite(sirena, LOW);
-      comprovar_estat();
-      if (estado1a == HIGH && estado1b == HIGH && estado2a == HIGH && estado2b == HIGH && estado3a == HIGH && estado3b == HIGH && estado4a == HIGH && estado4b == HIGH && estado5a == HIGH && estado5b == HIGH && estado6a == HIGH && estado6b == HIGH){
-        state = 0;
-      }else{
-        state = 2;
-      }
-      break;
-  }
+  delay(3000);
 }
